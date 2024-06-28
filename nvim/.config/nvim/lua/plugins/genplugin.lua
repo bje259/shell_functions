4e9f9b4b16edb2a150ab727113aa21dbd1965906
bje259 <bradley.euell@gmail.com>
2024-06-27 21:36:08 -0500

    tele submodule

--- nvim/.config/nvim/lua/plugins/genplugin.lua
+++ nvim/.config/nvim/lua/plugins/genplugin.lua
@@ -49,6 +49,18 @@ return {
     lazy = false,
   },
   {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      {
+        "agoodshort/telescope-git-submodules.nvim",
+        dependencies = "akinsho/toggleterm.nvim",
+      },
+    },
+    config = function()
+      require("telescope").load_extension("git_submodules")
+    end,
+  },
+  {
     "folke/tokyonight.nvim",
     lazy = true,
     opts = { style = "night" },
